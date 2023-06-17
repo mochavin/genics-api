7c18d438dab3489ce8af7cf8bb8b895d90ba3484
Moch. Avin <5025221061@mhs.its.ac.id>
2023-06-17 14:42:33 +0700

    tugas

---
+++ tugas.js
@@ -0,0 +1,66 @@
+const fetchData = async (endpoint) => {
+  const data = await fetch(endpoint);
+  const json = await data.json();
+  console.log(json);
+};
+
+const fetchData2 = async (endpoint) => {
+  try {
+    const data = await fetch(endpoint);
+    const json = await data.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function logUrlResult(url) {
+  try {
+    const response = await fetch(url);
+    console.log('URL:', url);
+    console.log('Status Code:', response.status);
+    const responseJSON = await response.json();
+    console.log('Response Content:');
+    console.log(responseJSON);
+  } catch (error) {
+    console.log('An error occurred:', error);
+  }
+}
+
+const logUrlResult2 = async (url) => {
+  try {
+    const response = await fetch(url);
+    console.log('URL:', url);
+    console.log('Status Code:', response.status);
+    const responseJSON = await response.json();
+    console.log('Response Content:');
+    console.log(responseJSON);
+    return responseJSON;
+  } catch (error) {
+    console.log('An error occurred:', error);
+  }
+};
+
+
+// Pilihan dari API yang tersedia
+const endpoint = 'https://jsonplaceholder.typicode.com/todos/';
+const apiURL = 'https://randomuser.me/api/';
+
+
+// Pilihan dari function
+
+// fetchData(endpoint);
+// fetchData2(endpoint);
+// logUrlResult(endpoint);
+// logUrlResult2(endpoint);
+
+// Pemanggilan function
+
+logUrlResult2(apiURL);
+// logUrlResult2(endpoint);
+
+
+// Contoh fetch tanpa function
+
+// const response = fetch(apiURL);
+// response.then((data) => data.json()).then((data) => console.log(data));
